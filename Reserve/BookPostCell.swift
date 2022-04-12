d1289b62912bc6cae5221554aa7e8770c3b70da3
Edward Cluster <ecluster@csumb.edu>
2022-04-12 16:11:00 -0700

    Created template for book catalog page

---
+++ Reserve/BookPostCell.swift
@@ -0,0 +1,33 @@
+//
+//  BookPostCell.swift
+//  Reserve
+//
+//  Created by Edward Cluster on 4/12/22.
+//
+
+import UIKit
+
+class BookPostCell: UITableViewCell {
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBAction func onCheckoutButton(_ sender: Any) {
+    }
+    @IBAction func onFavoriteButton(_ sender: Any) {
+    }
+    @IBAction func onCommentButton(_ sender: Any) {
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Reserve/CatalogViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CatalogViewController.swift
+//  Reserve
+//
+//  Created by Edward Cluster on 4/12/22.
+//
+
+import UIKit
+
+class CatalogViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookPostCell") as! BookPostCell
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
