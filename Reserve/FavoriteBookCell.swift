56bfa347b38689c05be46846b23d1f228d5c069f
Edward Cluster <ecluster@csumb.edu>
2022-04-19 17:19:12 -0700

    setting up favorite page and history page

---
+++ Reserve/FavoriteBookCell.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteBookCell.swift
+//  Reserve
+//
+//  Created by Edward Cluster on 4/19/22.
+//
+
+import UIKit
+
+class FavoriteBookCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var testLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Reserve/FavoriteViewController.swift
@@ -0,0 +1,45 @@
+//
+//  FavoriteViewController.swift
+//  Reserve
+//
+//  Created by Edward Cluster on 4/19/22.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteBookCell") as! FavoriteBookCell
+        
+        cell.testLabel.text = "Favorite Book"
+
+        return cell
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
